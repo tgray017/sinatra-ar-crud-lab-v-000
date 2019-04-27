91ba1e7d0574ffba9ee0675a018d399fc79568a8
Thomas Gray <tgray017@gmail.com>
2019-04-27 15:02:35 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,8 +36,17 @@ class ApplicationController < Sinatra::Base
     erb :edit
   end
   
-
+  patch '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.title = params[:title]
+    @article.content = params[:content]
+    @article.save
+    redirect to "/articles/#{@article.id}"
+  end
   
-  
-
+  delete '/articles/:id' do
+    @article = Article.find(params[:id])
+    @article.destroy
+    erb :index
+  end
 end
