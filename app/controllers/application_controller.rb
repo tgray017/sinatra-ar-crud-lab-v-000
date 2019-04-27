55b9974d69c4a4565288087988e3c42781bc70fc
Thomas Gray <tgray017@gmail.com>
2019-04-27 15:03:10 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -47,6 +47,6 @@ class ApplicationController < Sinatra::Base
   delete '/articles/:id' do
     @article = Article.find(params[:id])
     @article.destroy
-    erb :index
+    redirect to '/articles'
   end
 end
